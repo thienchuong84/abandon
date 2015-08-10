3e7877f29d79e59a1c13c430bf39681afb083315
thienchuong84 <thienchuong84@gmail.com>
2015-08-10 17:46:02 +0700

    abandon - mjn support
    Tạo col-md-3 , trong đó có panel -> list-group -> liệt kê các link :
    contact, tạo share, xvnkb
    Tạo file function khi click vào các link đó sẽ show ta col-md-9 kế bên
    nội dung các file
    Khi vào mjn.php sẽ load nội dung file mjn_contact.html trước bằng php
    include

--- mjn.php
+++ mjn.php
@@ -14,10 +14,12 @@ session_start();
 
   <!-- Bootstrap -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
-
   <style type="text/css">
-
+    #show_content b {
+      color: red;
+    }
   </style>
+  
 </head>
 <body>
 
@@ -29,25 +31,33 @@ session_start();
 
 
 
-  <div class="container-fluid">
+  <div class="container">
     <div class="row">
-      <div class="col-md-1"></div>
-      <div class="col-md-10">
-        <div class="row">
-          
-        </div> <!-- end row 1 -->
+    
 
-        <div class="row">
-          <div class="col-md-3"></div>
-
-          <div class="col-md-6">
-
+      <div class="col-md-3">
+        <!-- Khai báo 1 panel primary bao bọc list group cho đẹp -->
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Support
           </div>
+          <div class="panel-body">
+            <div class="list-group">
+              <a href="#" class="list-group-item" id="btn_3"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Contact</a> 
+              <a href="#" class="list-group-item" id="btn_1"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span> Tạo ổ đĩa share mj</a>
+              <a href="#" class="list-group-item" id="btn_2"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Mở Gõ Tiếng Việt (xvnkb)</a>              
+            </div>           
+          </div>
+        </div>
+      </div>
+    
 
-          <div class="col-md-3"></div>
-        </div><!-- end row 2 -->        
+      <div class="col-md-9"> 
+        <div id="show_content"><?php include 'mjn_contact.html'; ?></div>
       </div><!-- end div col-md-10 -->
-      <div class="col-md-1"></div>
+
+
+    </div>
   </div><!-- end .container-fluid -->
 
 
@@ -67,5 +77,6 @@ session_start();
 
   <script src="jquery/1.11.3/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery_load.js"></script>
 </body>
 </html>
