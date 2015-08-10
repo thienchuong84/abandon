e7107dc25c6616b51d2e039e9f087dda3277b685
thienchuong84 <thienchuong84@gmail.com>
2015-08-10 14:04:07 +0700

    abandon - inc_navbar.php
    + Viết tiếp phần query và show navbar khi user đăng nhập thành công
    + Trong login.php , khi có $_SESSION['ID'] , dùng hàm getArray() querry
    lấy navbar mà user có.
    + Xét nếu tồn tại biến session navbar này, ta mới kiểm tra biến này có
    trả về false ko? nếu ko thì loop bằng for để load nó ra. Tại sao ở đây
    ko dùng while như trong getArray? bởi vì trong đó có hàm mysqli_num_row
    nên ta có thể while được
    + Sau này sẽ viết function cho hàm load_navbar này nếu tồn tại session
    navbar. Nâng cao hơn nữa thì viết class User, trong class này có hàm
    navbar, navtab của nó ..

--- inc_navbar.php
+++ inc_navbar.php
@@ -10,12 +10,21 @@
 
 
     <?php // if isset($_SESSION['navbar']) -> Load navbar for user
-    //if(isset($_SESSION['navbar']) && $_SESSION['navbar'][0][0]!="") { 
-    if(isset($_SESSION['navbar'])) { ?>
-    <ul class="nav navbar-nav">      
-      <li class="active"><a href="mjn_abandon.php">Abandon</a></li>
-    </ul>
-    <?php } ?>
+    //if(isset($_SESSION['navbar']) && $_SESSION['navbar'][0][0]!="") {
+    /* Update Aug 10: sau khi đăng nhập từ trang login.php, nếu đăng nhập thành công sẽ trả về $_SESSION['id'], từ đó query để lấy các giá trị navbar mà user này có được
+    Trong login.php $_SESSION['navbar'] = getArray() lầy từ id user, tròng hàm này sẽ trả về array nếu có dữ liệu và trả về false nếu ko có dữ liệu. 
+    Vì vậy test thử if($_SESSION['navbar']) thay cho if(isset($_SESSION['navbar'])) , vì $_SESSION['navbar'] vẩn có thể có với biến là false 
+      Continue Update: nếu bỏ if(isset($_SESSION['navbar'])) sẽ gây ra lỗi trong trường hợp user chưa đăng nhập, vì vậy ta vẩn để điều kiện này, sau đó mới xét tiếp xem biến này có dữ liệu hay ko bằng cách kiểm tra nó true 
+      Next to: cần phải viết function cho navbar, sau khi kiểm tra nếu tồn tại $_SESSION['navbar'] này thì gọi function ra sử dụng */ 
+    if(isset($_SESSION['navbar'])) {
+      if ($_SESSION['navbar']) { 
+        $count = count($_SESSION['navbar']);
+        for($i=0; $i<$count; $i++) {  ?>
+          <ul class="nav navbar-nav">      
+            <!--<li class="active"><a href="mjn_abandon.php">Abandon</a></li>-->
+            <li class="active"><a href="<?php echo $_SESSION['navbar'][0][1]; ?>"><?php echo $_SESSION['navbar'][0][2]; ?></a>
+          </ul>
+    <?php }}} ?>
 
 
 
